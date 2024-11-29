afdd7d7bb17757d6e63ecad0b6c936ef693b72ab
Matthew <99048876+JustFiesta@users.noreply.github.com>
2024-11-29 21:17:50 +0100

    Fix Room compiler dependency
    Added kapt plugin and offical addnotation for it inside dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
@@ -53,7 +54,7 @@ dependencies {
 
     // Room database (SQL wrapper + ORM)
     implementation("androidx.room:room-runtime:$room_version")
-    implementation("androidx.room:room-compiler:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
     // optional - Kotlin Extensions and Coroutines support for Room
     implementation("androidx.room:room-ktx:$room_version")
     // optional - Test helpers
