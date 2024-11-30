40e0b5d8acefed4dfb11d38087594d38c141088f
Matthew <99048876+JustFiesta@users.noreply.github.com>
2024-11-30 16:03:57 +0100

    PIN validation implementation

--- app/src/main/java/com/focuszone/domain/Authenticator.kt
+++ app/src/main/java/com/focuszone/domain/Authenticator.kt
@@ -1,5 +1,30 @@
 package com.focuszone.domain
 
 // Class for authentication logic - PIN/Biometric
-class Authenticator {
+object Authenticator {
+
+
+    /* Validate pin from existing source
+    * input not is valid if...
+    * ... given input contains any character different than numbers
+    * ... given input is not equal to stored PIN
+    * ... given input is longer than 4
+    * ... given input is shorter than 4
+    * ... given input is empty
+    * ... given input is <= 0
+    * */
+    fun validatePin(userInput: String, validPin: String): Boolean {
+        // check if input is empty or contains characters different than numbers
+        if (userInput.isEmpty() || !userInput.all { it.isDigit() }) {
+            return false
+        }
+
+        // check input length
+        if (userInput.length != 4) {
+            return false
+        }
+
+        // check if input is valid
+        return userInput == validPin
+    }
 }
