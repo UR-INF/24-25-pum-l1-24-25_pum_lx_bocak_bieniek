bba00ed6150810f1881d5a2cc18a636f5d4e7ae6
Matthew <99048876+JustFiesta@users.noreply.github.com>
2024-11-30 14:30:27 +0100

    Logger singleton

--- app/src/main/java/com/focuszone/util/Logger.kt
+++ app/src/main/java/com/focuszone/util/Logger.kt
@@ -1,5 +1,41 @@
 package com.focuszone.util
 
-// Enable logging for debugging purposes
-class Logger {
+import android.util.Log
+
+object Logger {
+
+    // enable/disable
+    var isLoggingEnabled: Boolean = true
+
+    // DEBUG
+    fun debug(tag: String, message: String) {
+        if (isLoggingEnabled) {
+            Log.d(tag, message)
+        }
+    }
+
+    // INFO
+    fun info(tag: String, message: String) {
+        if (isLoggingEnabled) {
+            Log.i(tag, message)
+        }
+    }
+
+    // WARN
+    fun warn(tag: String, message: String) {
+        if (isLoggingEnabled) {
+            Log.w(tag, message)
+        }
+    }
+
+    // ERROR
+    fun error(tag: String, message: String, throwable: Throwable? = null) {
+        if (isLoggingEnabled) {
+            if (throwable != null) {
+                Log.e(tag, message, throwable)
+            } else {
+                Log.e(tag, message)
+            }
+        }
+    }
 }
