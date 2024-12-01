96d09003ea5b8049d63b190d1a92faf7599972e0
Matthew <99048876+JustFiesta@users.noreply.github.com>
2024-12-01 20:22:41 +0100

    Constans file to keep global naming in tact

---
+++ app/src/main/java/com/focuszone/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.focuszone.util
+
+object Constants {
+    const val SHARED_PREF_NAME = "focus_zone_user_preferences"
+    const val DATABASE_NAME = "focus_zone"
+    const val DEFAULT_MESSAGE = "Do you have time for that?"
+}
