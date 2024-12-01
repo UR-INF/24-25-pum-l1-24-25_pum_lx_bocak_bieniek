5fdee9526fd62d0181ced0ec4bc64ccece6feceb
Matthew <99048876+JustFiesta@users.noreply.github.com>
2024-12-01 20:21:35 +0100

    Monitors sketch

--- app/src/main/java/com/focuszone/domain/AppMonitor.kt
+++ app/src/main/java/com/focuszone/domain/AppMonitor.kt
@@ -1,5 +1,22 @@
 package com.focuszone.domain
 
 // monitor application activity if any was limited
-class AppMonitor {
+object AppMonitor {
+
+    /* Listen any opened app and check if its on the limited list
+    * */
+    fun monitorOpenedApps(){
+        // create app process listener
+        // fetch async list from DB
+        // compare if app is present in blocked list
+        // monitor/block app in loop
+    }
+
+    /* Block given app to the end of the day
+    * */
+    fun blockApp(){
+        // get app ID/name/package name/etc
+        // show fullscreen message
+        // tell system to block app
+    }
 }
--- app/src/main/java/com/focuszone/domain/NetworkMonitor.kt
+++ app/src/main/java/com/focuszone/domain/NetworkMonitor.kt
@@ -1,5 +1,13 @@
 package com.focuszone.domain
 
 // monitor network traffic if browser is used AND if any site was added
-class NetworkMonitor {
+object NetworkMonitor {
+
+    /* Listen to network traffic and check site is on blocked list
+    * */
+    fun monitorTraffic(){}
+
+    /* Block traffic to site
+    * */
+    fun blockTraffic(){}
 }
