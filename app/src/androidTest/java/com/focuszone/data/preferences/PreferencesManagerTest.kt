129814a5df393d201aad9316674e487646fde027
Matthew <99048876+JustFiesta@users.noreply.github.com>
2024-12-01 20:45:45 +0100

    Update PreferencesManagerTest to clear SharedPreferences before each test

--- app/src/androidTest/java/com/focuszone/data/preferences/PreferencesManagerTest.kt
+++ app/src/androidTest/java/com/focuszone/data/preferences/PreferencesManagerTest.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.focuszone.util.Constants.DEFAULT_MESSAGE
+import com.focuszone.util.Constants.SHARED_PREF_NAME
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,6 +23,11 @@ class PreferencesManagerTest {
     private val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
     private val preferencesManager = PreferencesManager(context)
 
+    @Before
+    fun resetPreferences() {
+        val sharedPreferences = context.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE)
+        sharedPreferences.edit().clear().apply()
+    }
 
     @Test
     fun `initial app return registration false`() {
