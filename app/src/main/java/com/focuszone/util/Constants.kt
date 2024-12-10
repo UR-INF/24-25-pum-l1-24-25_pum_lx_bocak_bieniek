43658f8086f29a81d1036345f3051fe8aae1302c
Matthew <99048876+JustFiesta@users.noreply.github.com>
2024-12-11 00:38:34 +0100

    Cleaned Constants

--- app/src/main/java/com/focuszone/util/Constants.kt
+++ app/src/main/java/com/focuszone/util/Constants.kt
@@ -2,6 +2,5 @@ package com.focuszone.util
 
 object Constants {
     const val SHARED_PREF_NAME = "focus_zone_user_preferences"
-    const val DATABASE_NAME = "focus_zone"
     const val DEFAULT_MESSAGE = "Do you have time for that?"
 }
