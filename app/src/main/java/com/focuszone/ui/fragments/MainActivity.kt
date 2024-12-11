2a4ec0933f0177ca750904cc5d4fc3c2b6579543
gubbl3bum <128317471+gubbl3bum@users.noreply.github.com>
2024-12-12 00:39:44 +0100

    Add Dark Mode logic

--- app/src/main/java/com/focuszone/ui/fragments/MainActivity.kt
+++ app/src/main/java/com/focuszone/ui/fragments/MainActivity.kt
@@ -2,6 +2,7 @@ package com.focuszone.ui.fragments
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.focuszone.R
@@ -10,6 +11,14 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
+        val sharedPreferences = getSharedPreferences("AppPreferences", 0)
+        val isDarkModeEnabled = sharedPreferences.getBoolean("dark_mode", false)
+        if (isDarkModeEnabled) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+
         setContentView(R.layout.activity_main)
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
--- app/src/main/java/com/focuszone/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/focuszone/ui/fragments/SettingsFragment.kt
@@ -1,19 +1,44 @@
 package com.focuszone.ui.fragments
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.focuszone.R
-import com.focuszone.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
-    private lateinit var binding: FragmentSettingsBinding
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        sharedPreferences = requireContext().getSharedPreferences("AppPreferences", 0)
+
+        val darkModeSwitch = view.findViewById<Switch>(R.id.switchDarkMode)
+
+        val isDarkModeEnabled = sharedPreferences.getBoolean("dark_mode", false)
+        darkModeSwitch.isChecked = isDarkModeEnabled
+        darkModeSwitch.text = if (isDarkModeEnabled) "Disable" else "Enable"
+
+        darkModeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                darkModeSwitch.text = "Disable"
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                darkModeSwitch.text = "Enable"
+            }
+
+            with(sharedPreferences.edit()) {
+                putBoolean("dark_mode", isChecked)
+                apply()
+            }
+        }
 
         val bttnAuthorization = view.findViewById<Button>(R.id.bttnAuthorization)
         bttnAuthorization.setOnClickListener {
