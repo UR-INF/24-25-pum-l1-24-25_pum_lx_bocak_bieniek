437fab2ad9fe13d05bd0872cb87e7cea51225b5b
Matthew <99048876+JustFiesta@users.noreply.github.com>
2025-01-05 17:42:04 +0100

    Refactor MainActivity placement

--- app/src/main/java/com/focuszone/ui/MainActivity.kt
+++ app/src/main/java/com/focuszone/ui/MainActivity.kt
@@ -1,4 +1,40 @@
 package com.focuszone.ui
 
-class MainActivity {
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.focuszone.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.util.Locale
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        val sharedPreferences = getSharedPreferences("AppPreferences", 0)
+        val isDarkModeEnabled = sharedPreferences.getBoolean("dark_mode", false)
+        if (isDarkModeEnabled) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+
+        val languageCode = sharedPreferences.getString("app_language", "en")
+        val locale = Locale(languageCode)
+        Locale.setDefault(locale)
+
+        val config = Configuration()
+        config.setLocale(locale)
+        resources.updateConfiguration(config, resources.displayMetrics)
+
+
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
+
+        bottomNavigationView.setupWithNavController(navController)
+    }
 }
