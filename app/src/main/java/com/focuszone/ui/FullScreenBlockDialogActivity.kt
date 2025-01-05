d9ccc477e9d979a1af0131da43d37baee01906b6
Matthew <99048876+JustFiesta@users.noreply.github.com>
2025-01-05 17:42:18 +0100

    Remove FullScreen dialog activity as it wont be needed

--- app/src/main/java/com/focuszone/ui/FullScreenBlockDialogActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.focuszone.ui
-
-// Fullscreen dialog
-// should show some customizable message and acknowledge button after limit was reached/blocked site detected
-class FullScreenBlockDialogActivity {
-}
