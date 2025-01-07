1f78ef763f55562e32fcc0a01e0ff2d271f3453a
gubbl3bum <128317471+gubbl3bum@users.noreply.github.com>
2025-01-07 10:10:27 +0100

    Add toast to login with biometric

--- app/src/main/java/com/focuszone/ui/fragments/WelcomeFragment.kt
+++ app/src/main/java/com/focuszone/ui/fragments/WelcomeFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.focuszone.R
@@ -30,6 +31,7 @@ class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
 
         val editPinLogin = view.findViewById<EditText>(R.id.editTextNumber)
         val bttnLogin = view.findViewById<Button>(R.id.bttnLogin)
+        val bttnLoginBiometric = view.findViewById<Button>(R.id.bttnLoginBiometric)
 
         bttnLogin.setOnClickListener {
             val inputPin = editPinLogin.text.toString()
@@ -38,6 +40,10 @@ class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
             } else {
                 showErrorDialog("Invalid PIN. Try again.")
             }
+        }
+
+        bttnLoginBiometric.setOnClickListener {
+            Toast.makeText(requireContext(), "Funkcja logowania przez biometrię nie jest jeszcze zaimplementowana.", Toast.LENGTH_SHORT).show()
         }
     }
 }
