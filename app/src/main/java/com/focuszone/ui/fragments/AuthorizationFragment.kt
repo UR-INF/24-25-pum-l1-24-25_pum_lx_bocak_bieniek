1b3e99e5aad8f7e81d5881f244f29876a056984d
gubbl3bum <128317471+gubbl3bum@users.noreply.github.com>
2025-01-10 14:07:42 +0100

    Delete unused files

--- app/src/main/java/com/focuszone/ui/fragments/AuthorizationFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.focuszone.ui.fragments
-
-import androidx.fragment.app.Fragment
-import com.focuszone.R
-
-class AuthorizationFragment : Fragment(R.layout.fragment_authorization) {
-}
--- app/src/main/java/com/focuszone/ui/fragments/PreferencesFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.focuszone.ui.fragments
-
-import androidx.fragment.app.Fragment
-import com.focuszone.R
-
-class PreferencesFragment : Fragment(R.layout.fragment_preferences){
-}
