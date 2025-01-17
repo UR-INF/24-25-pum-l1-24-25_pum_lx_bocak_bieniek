63859ee59833149145441a982a9dc763867e5958
Matthew <99048876+JustFiesta@users.noreply.github.com>
2025-01-17 17:26:10 +0100

    Comment out not used service

--- app/src/main/java/com/focuszone/domain/services/network/SiteBlockerService.kt
+++ app/src/main/java/com/focuszone/domain/services/network/SiteBlockerService.kt
@@ -19,73 +19,73 @@ import com.focuszone.util.Constants.KNOWN_BROWSERS
  *
  * It will start automatically as it is now System Service
  * **/
-
-class SiteBlockerService : AccessibilityService() {
-
-    private var preferenceManager = PreferencesManager(this)
-    private var blockedSites = preferenceManager.getBlockedSites()
-
-    /** On event - opened browser package - check URL in Preferences
-     * block if url is in Preferences
-     * "block" means GLOBAL_ACTION_BACK
-     * TODO
-     * **/
-    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
-        if (event == null) return
-
-        val packageName = event.packageName?.toString() ?: return
-
-        if (isBrowserPackage(packageName)) {
-            val nodeInfo = event.source ?: return
-
-            val url = extractUrlFromNode(nodeInfo)
-
-            if (url != null && isBlocked(url)) {
-                performGlobalAction(GLOBAL_ACTION_BACK)
-                Toast.makeText(applicationContext, "The website $url has been blocked by FocusZone.", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-    // Called when the service is interrupted
-    override fun onInterrupt() {
-        // Handle service interruption
-    }
-
-    // Checks if the app package is in KNOWS_BROWSERS
-    private fun isBrowserPackage(packageName: String): Boolean {
-        return KNOWN_BROWSERS.contains(packageName)
-    }
-
-    // Extract the URL from the node information
-    private fun extractUrlFromNode(nodeInfo: AccessibilityNodeInfo?): String? {
-        if (nodeInfo == null) return null
-
-        // Check if the node contains text that looks like a URL
-        if (nodeInfo.text != null && nodeInfo.text.toString().startsWith("http")) {
-            return nodeInfo.text.toString()
-        }
-
-        // Search the node's children
-        for (i in 0 until nodeInfo.childCount) {
-            val childNode = nodeInfo.getChild(i)
-            val url = extractUrlFromNode(childNode)
-            if (url != null) return url
-        }
-
-        return null
-    }
-
-    // Check if the URL is on the blocked list
-    private fun isBlocked(url: String): Boolean {
-        return blockedSites.any { blockedSite ->
-            url.startsWith(blockedSite.url.trim())
-        }
-    }
-
-    // Displays a message on the screen
-    private fun showToast(message: String) {
-        Handler(Looper.getMainLooper()).post {
-        }
-    }
-}
+//
+//class SiteBlockerService : AccessibilityService() {
+//
+//    private var preferenceManager = PreferencesManager(this)
+//    private var blockedSites = preferenceManager.getBlockedSites()
+//
+//    /** On event - opened browser package - check URL in Preferences
+//     * block if url is in Preferences
+//     * "block" means GLOBAL_ACTION_BACK
+//     * TODO
+//     * **/
+//    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+//        if (event == null) return
+//
+//        val packageName = event.packageName?.toString() ?: return
+//
+//        if (isBrowserPackage(packageName)) {
+//            val nodeInfo = event.source ?: return
+//
+//            val url = extractUrlFromNode(nodeInfo)
+//
+//            if (url != null && isBlocked(url)) {
+//                performGlobalAction(GLOBAL_ACTION_BACK)
+//                Toast.makeText(applicationContext, "The website $url has been blocked by FocusZone.", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//    }
+//
+//    // Called when the service is interrupted
+//    override fun onInterrupt() {
+//        // Handle service interruption
+//    }
+//
+//    // Checks if the app package is in KNOWS_BROWSERS
+//    private fun isBrowserPackage(packageName: String): Boolean {
+//        return KNOWN_BROWSERS.contains(packageName)
+//    }
+//
+//    // Extract the URL from the node information
+//    private fun extractUrlFromNode(nodeInfo: AccessibilityNodeInfo?): String? {
+//        if (nodeInfo == null) return null
+//
+//        // Check if the node contains text that looks like a URL
+//        if (nodeInfo.text != null && nodeInfo.text.toString().startsWith("http")) {
+//            return nodeInfo.text.toString()
+//        }
+//
+//        // Search the node's children
+//        for (i in 0 until nodeInfo.childCount) {
+//            val childNode = nodeInfo.getChild(i)
+//            val url = extractUrlFromNode(childNode)
+//            if (url != null) return url
+//        }
+//
+//        return null
+//    }
+//
+//    // Check if the URL is on the blocked list
+//    private fun isBlocked(url: String): Boolean {
+//        return blockedSites.any { blockedSite ->
+//            url.startsWith(blockedSite.url.trim())
+//        }
+//    }
+//
+//    // Displays a message on the screen
+//    private fun showToast(message: String) {
+//        Handler(Looper.getMainLooper()).post {
+//        }
+//    }
+//}
