071f5307cc7a35dd9d35c41b960be96242a83624
gubbl3bum <128317471+gubbl3bum@users.noreply.github.com>
2025-01-20 00:06:22 +0100

    Add logo to blocked apps list in home fragment

--- app/src/main/java/com/focuszone/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/focuszone/ui/fragments/HomeFragment.kt
@@ -1,5 +1,8 @@
 package com.focuszone.ui.fragments
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,15 +13,19 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.focuszone.R
+import com.focuszone.data.preferences.PreferencesManager
 import com.focuszone.data.preferences.entities.BlockedApp
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var blockedAppsAdapter: BlockedAppsAdapter
+    private lateinit var preferencesManager: PreferencesManager
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        preferencesManager = PreferencesManager(requireContext())
 
         recyclerView = view.findViewById(R.id.blockedAppsList)
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
@@ -30,7 +37,18 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     }
 
     private fun loadBlockedApps() {
+        val blockedApps = preferencesManager.getLimitedApps().map { app ->
+            app.copy(icon = fetchAppIcon(requireContext(), app.id))
+        }
 
+        blockedAppsAdapter.updateApps(blockedApps)
+    }
+    private fun fetchAppIcon(context: Context, packageName: String): Drawable? {
+        return try {
+            context.packageManager.getApplicationIcon(packageName)
+        } catch (e: PackageManager.NameNotFoundException) {
+            context.getDrawable(R.drawable.logo_focus_zone)
+        }
     }
 }
 
